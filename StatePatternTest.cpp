007d696a438371c539e028b2e15b0cb282f3b457
Jan Hecht <jan.hecht@jh-isw.de>
2019-05-16 20:15:32 +0200

    add another test

--- StatePatternTest.cpp
+++ StatePatternTest.cpp
@@ -1,21 +1,29 @@
 #include "MusicPlayer.h"
 #include "gtest/gtest.h"
 #include <string>
+//#include <iostream>
 
 class PlayerFixture : public testing::Test {
   protected:
     void SetUp() override {
       player = new MusicPlayer();
+      //std::cout << "SetUp" << std::endl;
     }
 
     void TearDown() override {
       delete player;
+      //std::cout << "TearDown" << std::endl;
     }
 
   MusicPlayer *player;
 };
 
 TEST_F(PlayerFixture, InitialStateIsStopped) {
+  ASSERT_STREQ("Stopped", player->GetState().c_str());
+}
+
+TEST_F(PlayerFixture, StoppedToPausedNotHappening){
+  player->Pause();
   ASSERT_STREQ("Stopped", player->GetState().c_str());
 }
 
