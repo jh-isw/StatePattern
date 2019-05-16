66e154871311983384d26eb79dee634a61c1ad5a
Jan Hecht <jan.hecht@jh-isw.de>
2019-05-16 19:33:49 +0200

    add fixture and test

--- StatePatternTest.cpp
+++ StatePatternTest.cpp
@@ -1,4 +1,23 @@
+#include "MusicPlayer.h"
 #include "gtest/gtest.h"
+#include <string>
+
+class PlayerFixture : public testing::Test {
+  public:
+    void SetUp() override {
+      player = new MusicPlayer();
+    }
+
+    void TearDown() override {
+      delete player;
+    }
+
+  MusicPlayer *player;
+};
+
+TEST_F(PlayerFixture, TestName) {
+  ASSERT_STREQ("Stopped", player->GetState().c_str());
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
